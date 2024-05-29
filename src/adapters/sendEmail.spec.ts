a708de38389933f878f8df380eb5683a43a859f7
cicomsa <ioanacomsa85@gmail.com>
2024-05-29 07:39:22 +0100

    sendEmail adapter added

---
+++ src/adapters/sendEmail.spec.ts
@@ -0,0 +1,35 @@
+import { sendEmail } from "./sendEmail";
+
+jest.useFakeTimers();
+jest.spyOn(global, "setTimeout");
+
+describe("sendEmail", () => {
+  afterEach(() => {
+    jest.clearAllTimers();
+    jest.clearAllMocks();
+  });
+
+  it("should return true when email was sent succesfully", async () => {
+    Math.random = jest.fn(() => 0.94);
+
+    const sendEmailPromise = sendEmail();
+
+    jest.runAllTimers();
+
+    const result = await sendEmailPromise;
+
+    expect(result).toBe(true);
+  });
+
+  it("should return false when emailfailed to be sent", async () => {
+    Math.random = jest.fn(() => 0.96);
+
+    const sendEmailPromise = sendEmail();
+
+    jest.runAllTimers();
+
+    const result = await sendEmailPromise;
+
+    expect(result).toBe(false);
+  });
+});
